8ef8b17491ad4a9a98caea642bd56741a6c54a6a
youngsan01 <intokys@gmail.com>
2024-07-29 09:39:25 +0900

    회원가입 및 레이아웃 구현중

---
+++ src/main/java/org/choongang/global/configs/FileConfig.java
@@ -0,0 +1,22 @@
+package org.choongang.global.configs;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(FileProperties.class)
+public class FileConfig implements WebMvcConfigurer {
+
+    private final FileProperties properties;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // /upload/**
+        registry.addResourceHandler(properties.getUrl()+ "**")
+                .addResourceLocations("file:///" + properties.getPath());
+    }
+}
